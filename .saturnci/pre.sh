6739249184ca60d6c5113a986362ba08b9ab90e8
JP Camara <jp.camara@wealthbox.com>
2026-01-16 00:11:48 -0500

    Fix Saturn CI: Add Node.js and JavaScript dependencies

--- .saturnci/pre.sh
+++ .saturnci/pre.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+set -e
+
+# Precompile assets using Vite
+echo "Precompiling assets..."
+bundle exec rails vite:build_all
+
+# Prepare the database
+echo "Preparing database..."
 bundle exec rails db:prepare
