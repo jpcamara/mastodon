118a7d2bfbb46b5c8d404d8c3220683712bb3d50
JP Camara <jp.camara@wealthbox.com>
2026-01-16 00:42:36 -0500

    A real will-they-wont-they ross-rachael moment

--- .saturnci/pre.sh
+++ .saturnci/pre.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Install JavaScript dependencies
+echo "Installing JavaScript dependencies..."
+yarn install
+
 # Precompile assets using Vite
 echo "Precompiling assets..."
 bundle exec rails vite:build_all
